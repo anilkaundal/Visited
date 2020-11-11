072973c0137ff23eaf9a4e8993b1acd755d1801d
Anil Kaundal <anilkaundal1999@gmail.com>
2020-11-11 22:40:36 +0530

    Change image to text

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // set view engine
 app.set("view engine", "ejs");
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('index');
