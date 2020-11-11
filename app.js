7e9b33c59554fe51488865a4adcf61f828885f63
Anil Kaundal <anilkaundal1999@gmail.com>
2020-11-11 21:56:52 +0530

    Replace local database to json-server-database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const https = require('https');
 const bodyParser = require('body-parser');
+const fetch = require("node-fetch");
 require('dotenv').config()
 
 const app = express();
@@ -17,11 +18,11 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-const url_loc = require('./locations.json')
+/*const url_loc = require('./locations.json')
 
 app.get('/locations', function (req, res) {
   res.json(url_loc);
-})
+})*/
 
 app.post('/', function (req, res) {
   var place = req.body.place;
@@ -32,8 +33,9 @@ app.post('/', function (req, res) {
       var locationData = JSON.parse(data);
       var latitude = Math.trunc(locationData.Response.View[0].Result[0].Location.DisplayPosition.Latitude);
       var longitude = Math.trunc(locationData.Response.View[0].Result[0].Location.DisplayPosition.Longitude);
-      // var location = { name: place, latitude: Math.trunc(latitude), longitude: Math.trunc(longitude) };
+      var location = { name: place.split(',')[0], lat: Math.trunc(latitude), long: Math.trunc(longitude) };
       // convert JSON object to string
+      /*
       fs.readFile('./locations.json', 'utf-8', (err, data) => {
         if (err) {
           throw err;
@@ -51,10 +53,36 @@ app.post('/', function (req, res) {
           }
           console.log('JSON data is saved.');
         });
-      });
+      });*/
+      // Send this object as JSON data to the server
+      Promise.all([
+        fetch("https://locations-database.herokuapp.com/locations", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(location)
+        })
+          // fetch("https://locations-database.herokuapp.com/locations")
+        ])
+        /*
+          .then((results) => results.json())
+          .then((data) => data.forEach((element) => {
+            if (element.name == del) {
+              const url_delete = "https://locations-database.herokuapp.com/locations?name=" + element.id;
+              fetch(url_delete, {
+                method: "DELETE",
+              })
+            }
+          }))*/
+          .catch(function (error) {
+            // if there's an error, log it
+            console.log(error);
+          });
       res.redirect('/');
     })
-  });
+  })
 });
 
 var removeByAttr = function (arr, attr, value) {
